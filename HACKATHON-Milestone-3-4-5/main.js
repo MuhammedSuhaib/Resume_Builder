6df6c9a04bb362144cbcb5f173b60c042e04c12e
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2026-02-05 00:25:12 +0500

    lil more improvements

--- HACKATHON-Milestone-3-4-5/main.js
+++ HACKATHON-Milestone-3-4-5/main.js
@@ -1,8 +1,8 @@
 function toggleDarkMode() {
     document.body.classList.toggle('dark');
-    var button = document.querySelector('button');
-    if (button) {
-        button.textContent = document.body.classList.contains('dark')
+    var darkModeButton = document.querySelector('button'); // First button is dark mode
+    if (darkModeButton && !darkModeButton.textContent.includes('Download')) {
+        darkModeButton.textContent = document.body.classList.contains('dark')
             ? 'Light Mode'
             : 'Dark Mode';
     }
